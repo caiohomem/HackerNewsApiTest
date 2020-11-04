3c761bed061ea959b12913b71bb6c32c3032d05c
Caio Chagas <caio.chagas@goinside.co>
2020-11-04 16:53:00 +0000

    Versão Inicial

---
+++ HackerNewsApiTest/HackerNewsApiTest/Controllers/HackerNewsApiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackerNewsApiTest.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HackerNewsApiController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        [Route("beststories")]
+        public ActionResult<IEnumerable<string>> GetBestStories()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
